9d2ae97894f344448e5b878dc18f7581f060366d
enesfejza76 <enesfejza76@gmail.com>
2024-12-07 00:03:00 +0100

    Removing Course container

--- src/Components/templates/Course/Course.js
+++ src/Components/templates/Course/Course.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./Course.css";
-import CourseContainer from "../../organisms/CourseContainer/CourseContainer";
 import CourseTitle from "../../atoms/CourseTitle/CourseTitle";
 
 export default function Course() {
@@ -8,22 +7,7 @@ export default function Course() {
         <div className="courseCotainerMiddle">
             <CourseTitle title="Html and css Course" />
             <div className="courseBoxContainer">
-                <CourseContainer style={{
-                    width: "20%"
-                }}
-                    courseTitle="Html and CSS"
-                    styleTitle={{
-                        fontSize: "1.7rem"
-                    }}
-                />
-                <CourseContainer style={{
-                    width: "80%"
-                }}
-                    courseTitle="Html Introduction"
-                    styleTitle={{
-                        fontSize: "1.7rem"
-                    }}
-                />
+
             </div>
         </div>
     </div>
